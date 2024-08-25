a3438e781ee1702f87ec4df1a4b1f7b563691155
Bungeefan <severin.hamader@yahoo.de>
2024-08-26 00:12:15 +0200

    Upgraded example

--- example/android/app/src/main/java/com/example/flutter_multi_long_press_example/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_multi_long_press_example;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ example/android/app/src/main/kotlin/com/example/multi_long_press/example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.multi_long_press.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
